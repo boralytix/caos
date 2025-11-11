b089230bb14217319507f61fd4e1adadbcefda26
Nastya_00 <ad.soboleva1101@gmail.com>
2025-11-11 20:20:00 +0300

    Update solution02_02.s

--- hw/lab02/02_countdown/solution02_02.s
+++ hw/lab02/02_countdown/solution02_02.s
@@ -2,3 +2,19 @@
 .text
 .globl  main
 
+.include "simpleio_x86_64.S"
+
+main:
+    call readi32
+    cmp eax, 0
+    jl finish
+    mov ecx, eax
+
+loop:
+    mov eax, ecx
+    call writei32
+    call nl
+    cmp ecx, 0
+    je finish
+    dec ecx
+    jmp loop
